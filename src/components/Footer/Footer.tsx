2d4d04c0b3ca92ec9f26b3d9508af8a890aa6341
vertexofvortex <tbm92ru@gmail.com>
2024-03-21 16:48:57 +0300

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,28 @@
 import { FC } from "react"
+import s from "./Footer.module.scss"
 
 const Footer: FC = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer>
+      <div className={s.wrapper}>
+        <div className={s.copyright}>© Soviet Girls 2024</div>
+        <div className={s.links}>
+          <a href="https://sovietgirls.su/license.pdf" target="_blank">
+            Лицензионное соглашение
+          </a>
+          <a href="https://market.sovietgirls.su" target="_blank">
+            Маркетплейс
+          </a>
+        </div>
+        <div className={s.contacts}>
+          <div>help@sovietgirls.su</div>
+          <a href="https://vk.com/sovietgirls_nft">
+            <img src="/images/vk.svg" alt="" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
