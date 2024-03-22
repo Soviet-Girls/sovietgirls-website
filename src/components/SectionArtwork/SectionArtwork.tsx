f870f94fb6e7c46a40cc3d4200cd9fff6c387f27
vertexofvortex <tbm92ru@gmail.com>
2024-03-22 16:25:41 +0300

    Scroll button behavior

--- src/components/SectionArtwork/SectionArtwork.tsx
+++ src/components/SectionArtwork/SectionArtwork.tsx
@@ -1,9 +1,11 @@
-import { FC } from "react"
+import { FC, useRef } from "react"
 import s from "./SectionArtwork.module.scss"
 
 const SectionArtwork: FC = () => {
+  const artworkRef = useRef<HTMLElement>(null)
+
   return (
-    <section className={s.artwork}>
+    <section className={s.artwork} ref={artworkRef}>
       <div className={s.artworkContainer}>
         <img src="/images/artwork.svg" alt="" />
         <div className={s.caption}>
@@ -14,7 +16,19 @@ const SectionArtwork: FC = () => {
       </div>
       <div className={s.artworkContainerMobile}>
         <img src="/images/artwork-mobile.png" alt="" className={s.cover} />
-        <img src="/images/arrow-down.svg" alt="" className={s.arrow} />
+        <img
+          src="/images/arrow-down.svg"
+          alt=""
+          className={s.arrow}
+          onClick={() =>
+            artworkRef &&
+            scrollTo({
+              top:
+                (artworkRef.current?.getBoundingClientRect().height || 50) - 50,
+              behavior: "smooth",
+            })
+          }
+        />
       </div>
     </section>
   )
